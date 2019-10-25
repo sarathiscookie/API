a9c8290dacc060b2000412ddf2ed3ecc25abfefb
Sarath TS <sarathiscookie@hotmail.com>
2019-10-25 11:16:16 +0200

    Updated module details migration

--- database/migrations/2019_10_22_075708_create_module_details_table.php
+++ database/migrations/2019_10_22_075708_create_module_details_table.php
@@ -17,23 +17,24 @@ class CreateModuleDetailsTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('module_id');
             $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('company_id');
             $table->tinyInteger('status')->default(0); // Status to enable and disable module_details
+            $table->integer()->nullable();// Wait with execution until the MOD pointer number is reached
+            $table->integer()->nullable();// Wait until MOD has successfully completed with ID
             $table->dateTime('last_call')->nullable(); // When was cron job last call 
             $table->tinyInteger('call_count')->default(0); // How many times cron job called
             $table->tinyInteger('run_count')->default(0); // How many times cron runned
             $table->dateTime('last_error')->nullable(); // When was cron failed
             $table->tinyInteger('error_count')->default(0); // How many times cron failed
             $table->integer('max_error')->nullable(); // It is for setting maximum error limit
-            $table->string('delivery_status', '50')->nullable();
+            $table->tinyInteger('delivery_status')->default(0); // 0 not active, 1 active, 2 wait
             $table->tinyInteger('order_in_logistics')->default(0); // Place order as set order in logistics
             $table->tinyInteger('order_shipped')->default(0); // Declare order as shipped
             $table->tinyInteger('status_mail')->default(0); // Email status
             $table->string('mail_from_name')->nullable();
-            $table->string('mail_address')->nullable();
-            $table->string('mail_address_name')->nullable();
-            $table->string('mail_cc')->nullable();
-            $table->string('mail_cc_name')->nullable();
-            $table->string('mail_bcc')->nullable();
+            $table->unsignedBigInteger('user_id'); // To get supplier name and email
+            $table->unsignedBigInteger('user_id'); // To get admin name and email
+            $table->string('mail_bcc')->nullable(); 
             $table->string('mail_bcc_name')->nullable();
             $table->string('mail_subject')->nullable();
             $table->text('mail_body')->nullable();
