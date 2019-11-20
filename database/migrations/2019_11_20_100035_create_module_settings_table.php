42459e8b1fafd425f48db588feaccc0bddebbfa5
sarathts <sarathiscookie@hotmail.com>
2019-11-20 15:36:42 +0530

    Updated migrations

--- database/migrations/2019_11_20_100035_create_module_settings_table.php
+++ database/migrations/2019_11_20_100035_create_module_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModuleDetailsTable extends Migration
+class CreateModuleSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -67,6 +67,6 @@ class CreateModuleDetailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('module_details');
+        Schema::dropIfExists('module_settings');
     }
 }
